dbab34aef0aa75eaff9b797bcfa4c92d1aa6e3a6
Mike <mike.ye@klm.com>
2022-05-03 10:51:04 +0200

    anonymous inner class attempt

--- src/ExternalActionListener.java
+++ src/ExternalActionListener.java
@@ -4,18 +4,10 @@ public class ExternalActionListener implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent event) {
-        String actionCommand = event.getActionCommand();
+//        String actionCommand = event.getActionCommand();
 
-        switch (actionCommand) {
-            case "1":
-                RekenMachine.displayField.setText("1");
-                break;
-            case "2":
-                break;
-            case "3":
-                break;
+        RekenMachine.displayField.setText(event.getActionCommand());
 
-        }
     }
 
 }
